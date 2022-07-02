7779f3572e0fad491c3e8899706fac21b43d83e4
Em3raud3 <Animekamisama@gmail.com>
2022-07-02 17:34:10 -0400

    barely functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   today = new Date();
+
+  ngOnInit(): void {
+    $('input').datetimepicker();
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
 }
